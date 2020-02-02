f08faa6e55a26c0b40de321bbba3ee1f5e8567a3
Stefan Broenner <sbroenne@Stefans-MBP.fritz.box>
2020-02-02 22:32:32 +0100

    New release

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./create_repository.py https://github.com/sbroenne/plugin.video.nhkworldtv
+./create_repository.py --compressed https://github.com/sbroenne/plugin.video.nhkworldtv https://github.com/sbroenne/repository.sbroenne
