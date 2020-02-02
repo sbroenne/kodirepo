a117b3ae14123cb9677b9ce85f078b8121c6b525
Stefan Broenner <sbroenne@Stefans-MBP.fritz.box>
2020-02-02 17:57:00 +0100

    Initial commit

--- build.cmd
+++
@@ -1,2 +0,0 @@
-rem create_repository.py https://github.com/sbroenne/plugin.video.nhkworldtv
-create_repository.py https://github.com/sbroenne/plugin.video.nhkworldtv
---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./create_repository.py https://github.com/sbroenne/plugin.video.nhkworldtv
