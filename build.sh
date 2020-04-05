cae2d0adb0f2b8e713e3acf44313dfeedb89478c
Stefan Broenner <sbroenne@Stefans-MBP.fritz.box>
2020-04-05 16:27:12 +0200

    Added a new version of requests cache

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./create_repository.py --compressed https://github.com/sbroenne/plugin.video.nhkworldtv https://github.com/sbroenne/repository.sbroenne
+./create_repository.py --compressed https://github.com/sbroenne/plugin.video.nhkworldtv https://github.com/sbroenne/repository.sbroenne https://github.com/sbroenne/script.module.requests-cache
