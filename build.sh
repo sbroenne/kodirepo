b97568d2a32916114c997ea4ce389aee13324e53
sbroenne <stefan_broenner@yahoo.com>
2020-12-11 14:18:34 +0100

    Release 0.0.125

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Package NHK World TV Plug-in
 rm -rf ./plugin.video.nhkworldtv/plugin.video.nhkworldtv-*
-./create_repository.py --compressed ../plugin.video.nhkworldtv/build/leia/plugin.video.nhkworldtv https://github.com/sbroenne/repository.sbroenne
+./create_repository.py --compressed  https://github.com/sbroenne/plugin.video.nhkworldtv.git/:plugin.video.nhkworldtv https://github.com/sbroenne/repository.sbroenne
---
+++ build_local.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Package NHK World TV Plug-in
+rm -rf ./plugin.video.nhkworldtv/plugin.video.nhkworldtv-*
+./create_repository.py --compressed ../plugin.video.nhkworldtv/build/leia/plugin.video.nhkworldtv https://github.com/sbroenne/repository.sbroenne
