37f61bf78940f2d1aae9fa9e488dc59469059beb
sbroenne <stefan_broenner@yahoo.com>
2021-12-30 11:46:01 +0100

    Renoved old script

--- build_local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Package NHK World TV Plug-in
-rm -rf ./plugin.video.nhkworldtv/plugin.video.nhkworldtv-*
-
-pipenv update
-pipenv run ./create_repository.py --compressed ../plugin.video.nhkworldtv/build/matrix/plugin.video.nhkworldtv https://github.com/sbroenne/repository.sbroenne
