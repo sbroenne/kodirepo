3424f4334b1e01c80c7c3cede5afc5e5000ecce7
sbroenne <stefan_broenner@yahoo.com>
2021-12-30 11:45:44 +0100

    Build plugin repo from GitHub instead of local

---
+++ update_plugin_repo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Package NHK World TV Plug-in
+
+# Create repository from GitHub
+pipenv run ./create_repository.py --compressed https://github.com/sbroenne/plugin.video.nhkworldtv.git:plugin.video.nhkworldtv
---
+++ update_sbroenne_repo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Package NHK World TV Plug-in
+
+# Create repository from GitHub
+pipenv run ./create_repository.py --compressed https://github.com/sbroenne/repository.sbroenne
+
